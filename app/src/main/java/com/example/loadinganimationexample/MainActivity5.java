cf02ba11b4ee6c8ea75a69c137dbba7e1e7e2487
kim-da-hye <ioiioi0412@daum.net>
2021-01-13 20:41:55 +0900

    Bus

--- app/src/main/java/com/example/loadinganimationexample/MainActivity5.java
+++ app/src/main/java/com/example/loadinganimationexample/MainActivity5.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.CompoundButton;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
@@ -11,13 +12,9 @@ import android.widget.Toast;
 
 public class MainActivity5 extends AppCompatActivity {
 
-    private RadioButton single_list_item_check_button,second_single_list_item_check_button
-            ,forth_single_list_item_check_button, third_single_list_item_check_button,fifth_single_list_item_check_button,
-            sixth_single_list_item_check_button,seventh_single_list_item_check_button,eighth_single_list_item_check_button,
-            ninth_single_list_item_check_button,tenth_single_list_item_check_button, twelvth_single_list_item_check_button,
-            eleventh_single_list_item_check_button,thirdteenth_single_list_item_check_button;
-    private RadioGroup radioGroup;
-    private String str_result;
+    private RadioButton single_list_item_check_button;
+    private View.OnClickListener radioButtonClickListener;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,21 +22,21 @@ public class MainActivity5 extends AppCompatActivity {
         setContentView(R.layout.activity_main5);
         ActionBar actionBar=getSupportActionBar();
         actionBar.hide();
-        radioGroup=findViewById(R.id.radioGroup);
-        single_list_item_check_button=findViewById(R.id.single_list_item_check_button);
-        second_single_list_item_check_button=findViewById(R.id.second_single_list_item_check_button);
 
-        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+        single_list_item_check_button=(RadioButton) findViewById(R.id.single_list_item_check_button);
+        
+        single_list_item_check_button.setOnClickListener(radioButtonClickListener);
 
-            @Override
-            public void onCheckedChanged(RadioGroup group, int i) {
-                if(i == R.id.single_list_item_check_button){
-                    Toast.makeText(MainActivity5.this,"1정거장",Toast.LENGTH_SHORT).show();
-                } else if (i == R.id.single_list_item_check_button){
-                    Toast.makeText(MainActivity5.this,"2정거장",Toast.LENGTH_SHORT).show();
-                }
-            }
-        });
+
+
+        RadioButton.OnClickListener radioButtonClickListener = new RadioButton.OnClickListener(){
+            @Override public void onClick(View view) {
+                Toast.makeText(MainActivity5.this, "첫번째 정류장 : "+single_list_item_check_button.isChecked(), Toast.LENGTH_SHORT).show();
+            } };
+
+
+
+
 
 
     }
--- app/src/main/java/com/example/loadinganimationexample/SubActivity.java
+++ app/src/main/java/com/example/loadinganimationexample/SubActivity.java
@@ -1,8 +1,5 @@
 package com.example.loadinganimationexample;
 
-import androidx.appcompat.app.ActionBar;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
@@ -10,24 +7,34 @@ import android.text.Editable;
 import android.text.TextWatcher;
 import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
 
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+
 public class SubActivity extends AppCompatActivity{
-    ListView listview = null ;
-    Button button2;
+    ListView listview_t;
+    Button button_t;
+    EditText editTextFilter;
+    ArrayAdapter<String> adapter;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_sub);
         ActionBar actionBar = getSupportActionBar();
         getSupportActionBar().setTitle(" 컴소버스");
         actionBar.setBackgroundDrawable(new ColorDrawable(0xFF6495ED));
         ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1) ;
-        listview = (ListView) findViewById(R.id.listview1) ;
-        listview.setAdapter(adapter) ;
+        listview_t = (ListView) findViewById(R.id.listview_t) ;
+        editTextFilter = (EditText)findViewById(R.id.editTextFilter) ;
+        button_t=(Button)findViewById(R.id.button_t);
+        listview_t.setAdapter(adapter) ;
         adapter.add("100") ;
         adapter.add("201") ;
         adapter.add("302") ;
@@ -39,7 +46,7 @@ public class SubActivity extends AppCompatActivity{
         adapter.add("909") ;
         adapter.add("1110") ;
 
-        EditText editTextFilter = (EditText)findViewById(R.id.editTextFilter) ;
+
         editTextFilter.addTextChangedListener(new TextWatcher() {
 
 
@@ -48,9 +55,9 @@ public class SubActivity extends AppCompatActivity{
             public void afterTextChanged(Editable edit) {
                 String filterText = edit.toString() ;
                 if (filterText.length() > 0) {
-                    listview.setFilterText(filterText) ;
+                    listview_t.setFilterText(filterText) ;
                 } else {
-                    listview.clearTextFilter() ;
+                    listview_t.clearTextFilter() ;
                 }
             }
 
@@ -62,13 +69,15 @@ public class SubActivity extends AppCompatActivity{
             public void onTextChanged(CharSequence s, int start, int before, int count) {
             }
         }) ;
-        button2.setOnClickListener(new View.OnClickListener() {
+
+
+
+        button_t.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 Intent intent = new Intent(SubActivity.this, MainActivity3.class);
                 startActivity(intent);
             }
         });
-
 
 
     }
